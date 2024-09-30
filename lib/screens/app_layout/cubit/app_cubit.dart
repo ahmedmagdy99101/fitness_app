5646e40e84ebe44fc90c4878741d0d21addc184f
Ahmed Magdy <44295316+ahmedmagdy99101@users.noreply.github.com>
2024-10-01 01:27:14 +0300

    add app layout to start the project

--- lib/screens/app_layout/cubit/app_cubit.dart
+++ lib/screens/app_layout/cubit/app_cubit.dart
@@ -1,32 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../home_screen/home_screen.dart';
 import 'app_states.dart';
 
-
 class AppCubit extends Cubit<AppStates> {
   AppCubit() : super(InitialState());
+
   static AppCubit get(context) => BlocProvider.of(context);
 
-  bool isDarkTheme = false ;
+  bool isDarkTheme = false;
+
   void changeTheme() {
-    state is DarkThemeState ? isDarkTheme = true : isDarkTheme = false ;
+    state is DarkThemeState ? isDarkTheme = true : isDarkTheme = false;
     emit(state is LightThemeState ? DarkThemeState() : LightThemeState());
     print(isDarkTheme);
   }
 
-  int bottomNavIndex =0 ;
-  List<Widget> bottomNavPages =  [
-     const HomeScreen(),
-       Container(),
-       Container(),
-       Container(),
+  int bottomNavIndex = 0;
+
+  List<Widget> bottomNavPages = [
+    const HomeScreen(),
+    Container(),
+    Container(),
+    Container(),
   ];
+
   void changeBottomNavbar(index) {
     bottomNavIndex = index;
     emit(ChangeBottomNavbarState());
   }
-
 }
