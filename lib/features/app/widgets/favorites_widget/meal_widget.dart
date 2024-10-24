7fc7410f0aed2a222bd4f14fa00c993bd3d0269e
suhailafarahat <suhailafarahat8@gmail.com>
2024-10-24 12:38:36 +0300

    edit font

--- lib/features/app/widgets/favorites_widget/meal_widget.dart
+++ lib/features/app/widgets/favorites_widget/meal_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MealWidget extends StatelessWidget {
-  const MealWidget({super.key});
+  final List<Meal> meals;
+
+  const MealWidget({Key? key, required this.meals}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,61 +11,76 @@ class MealWidget extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView.builder(
-          itemBuilder: (BuildContext context, int index) =>
-              mealCard(),
-          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => mealCard(meals[index]),
+          itemCount: meals.length,
         ),
       ),
     );
   }
 
-  Widget mealCard() =>
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+  Widget mealCard(Meal meal) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Image.asset(
+          meal.imagePath,
+          height: 200,
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+      ),
+      Text(
+        meal.name,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Row(
         children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Image.asset(
-                "assets/images/meal.png",
-                height: 200,
-                width: double.infinity,
-                fit: BoxFit.fill,
-              )),
-          const Text(
-            'Green beans, tomatoes, eggs',
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          const Icon(
+            Icons.local_fire_department_outlined,
+            color: Colors.blueAccent,
+            size: 20,
           ),
-          const Row(
-            children: [
-              Icon(
-                Icons.local_fire_department_outlined,
-                color: Colors.blueAccent,
-                size: 20,
-              ),
-              Text(
-                '135 kcal',
-                style: TextStyle(color: Colors.grey, fontSize: 16),
-              ),
-              Text('  |  '),
-              Icon(
-                Icons.access_time,
-                color: Colors.blueAccent,
-                size: 20,
-              ),
-              Text(
-                ' 30 min',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                ),
-              ),
-            ],
+          Text(
+            meal.calories,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
           ),
-          const SizedBox(
-            height: 40,
-          )
+          const Text('  |  '),
+          const Icon(
+            Icons.access_time,
+            color: Colors.blueAccent,
+            size: 14,
+          ),
+          Text(
+            meal.time,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 14,
+            ),
+          ),
         ],
-      );
+      ),
+      const SizedBox(
+        height: 40,
+      ),
+    ],
+  );
+}
 
+class Meal {
+  final String imagePath;
+  final String name;
+  final String calories;
+  final String time;
+
+  Meal({
+    required this.imagePath,
+    required this.name,
+    required this.calories,
+    required this.time,
+  });
 }
